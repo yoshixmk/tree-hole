5435fc12eea0d32ea2446eacd82eb5ed16d17213
y.ueki <y.ueki@astamuse.co.jp>
2020-05-04 12:41:34 +0900

    nextビルドが終わらない問題を解消

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -27,11 +27,12 @@ const WithStaticProps = ({ items }: Props): JSX.Element =>
   </Layout>;
 
 
+// eslint-disable-next-line @typescript-eslint/require-await
 export const getStaticProps: GetStaticProps = async () => {
   // Example for including static props in a Next.js function component page.
   // Don't forget to include the respective types for any props passed into
   // the component.
-  const items: Array<User> = await new Promise(() => sampleUserData);
+  const items: Array<User> = sampleUserData;
   return { props: { items } };
 };
 
