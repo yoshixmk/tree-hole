c403668f7eeef02d9bfdbe565f7d04c9879a7f80
yoshixmk <pirori67@gmail.com>
2021-02-06 16:59:16 +0900

    lint setting

--- README.md
+++ README.md
@@ -5,3 +5,13 @@
 ```bash
 $ pipenv run start
 ```
+
+## How to
+1. Wording
+   1. 日本語教育推進法
+   2. 日本語教育推進基本法
+   3. 日本語教育の推進に関する法律
+     1. 日本語教育の推進
+   4. 日本語教育の機会
+2. Targetting
+   1. https://scholar.google.com/
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from scholarly import scholarly
 
-# Retrieve the author's data, fill-in, and print
 search_query = scholarly.search_author('Steven A Cholewiak')
+# search_query = scholarly.search_keyword('Haptics')
 author = scholarly.fill(next(search_query))
+# scholarly.pprint(next(search_query))
 print(author)
 
 # Print the titles of the author's publications
